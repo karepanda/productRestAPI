74610a6e4e9aa951486a0c66f367c0cf69284a43
kare <karelysperezs@gmail.com>
2025-07-15 16:35:22 +0200

    add JPA SQL logging and enhance Product entity tests

--- src/test/java/org/application/core/producttestapi/ProductTestApiApplicationTests.java
+++ src/test/java/org/application/core/producttestapi/ProductTestApiApplicationTests.java
@@ -2,6 +2,7 @@ package org.application.core.producttestapi;
 
 import org.application.core.producttestapi.entities.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,36 +10,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ProductTestApiApplicationTests {
 
-
-    @Test
-    public void testGetProduct() {
-        TestRestTemplate restTemplate = new TestRestTemplate();
-        Product product = restTemplate.getForObject("http://localhost:8080/products/1", Product.class);
-
-        assertNotNull(product);
-        assertEquals("MacBook pro air", product.getName());
-    }
+    @Value("${producttestapi.services.url}")
+    private String baseURl;
 
     @Test
     public void testCreateProduct() {
         TestRestTemplate restTemplate = new TestRestTemplate();
         Product newProduct = new Product();
-        newProduct.setName("iPhone 14");
+        newProduct.setName("iPhone 16");
         newProduct.setDescription("Latest Apple iPhone");
-        newProduct.setPrice(999);
+        newProduct.setPrice(1999);
 
-        Product createdProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", newProduct, Product.class);
+        Product createdProduct = restTemplate.postForObject(baseURl, newProduct, Product.class);
 
         assertNotNull(createdProduct);
-        assertEquals("iPhone 14", createdProduct.getName());
+        assertEquals("iPhone 16", createdProduct.getName());
     }
+
+    @Test
+    public void testGetProduct() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        Product product = restTemplate.getForObject(baseURl + "6", Product.class);
+
+        assertNotNull(product);
+        assertEquals("iPhone 16", product.getName());
+    }
+
 
     @Test
     public void testUpdateProduct() {
         TestRestTemplate restTemplate = new TestRestTemplate();
-        Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1", Product.class);
+        Product product = restTemplate.getForObject(baseURl + "6", Product.class);
 
-        product.setPrice(1200);
+        product.setPrice(900);
 
         restTemplate.put("http://localhost:8080/productapi/products/", product);
 
