c9a66fd944e6745d4ac8d8972ac0ab93119fff46
kare <karelysperezs@gmail.com>
2025-08-15 14:22:48 +0200

    refactor ProductControllerTest to improve readability and add JSON response validation

--- src/test/java/org/application/core/producttestapi/controllers/ProductControllerTest.java
+++ src/test/java/org/application/core/producttestapi/controllers/ProductControllerTest.java
@@ -1,5 +1,7 @@
 package org.application.core.producttestapi.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.application.core.producttestapi.entities.Product;
 import org.application.core.producttestapi.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
@@ -15,9 +17,17 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(ProductController.class)
 class ProductControllerTest {
+
+    private static final int PRODUCT_ID = 1;
+    public static final String PRODUCT_NAME = "iPhone 16";
+    public static final String PRODUCT_DESCRIPTION = "Its Awesome";
+    public static final int PRODUCT_PRICE = 1400;
+    public static final String PRODUCT_CONTEXT_PATH = "/productapi";
+    public static final String PRODUCTS_URL = "/productapi/products/";
 
     @Autowired
     private MockMvc mockMvc;
@@ -27,19 +37,27 @@ class ProductControllerTest {
 
     @Test
     public void testFindAllProducts() throws Exception {
-        Product product = new Product();
-        product.setId(1);
-        product.setName("iPhone 16");
-        product.setDescription("Its Awesome");
-        product.setPrice(1400);
+        Product product = buildProduct();
 
         List<Product> products = List.of(product);
 
         when(repository.findAll()).thenReturn(products);
 
-        mockMvc.perform(get("/productapi/products/")
-            .contextPath("/productapi"))
-            .andExpect(status().isOk());
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+        mockMvc.perform(get(PRODUCTS_URL)
+            .contextPath(PRODUCT_CONTEXT_PATH))
+            .andExpect(status().isOk())
+            .andExpect(content().json(objectWriter.writeValueAsString(products)));
+    }
+
+    private static Product buildProduct() {
+        Product product = new Product();
+        product.setId(PRODUCT_ID);
+        product.setName(PRODUCT_NAME);
+        product.setDescription(PRODUCT_DESCRIPTION);
+        product.setPrice(PRODUCT_PRICE);
+        return product;
     }
 
 }
